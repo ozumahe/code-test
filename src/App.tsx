f69ecc84fe8eafaa148b0cadb7a0b2717d459ee8
Ozumah Ebenezer <ozumahe@gmail.com>
2023-08-06 21:56:08 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+type UserType = {
+  id: number;
+  userName: string;
+  phoneNumber: string;
+};
 
 function App() {
+  const [users, setUsers] = useState<UserType[]>([
+    {
+      id: 1,
+      userName: "Ozumahe",
+      phoneNumber: "7397873082878",
+    },
+    {
+      id: 2,
+      userName: "Micheal",
+      phoneNumber: "7397873082878",
+    },
+    {
+      id: 3,
+      userName: "Job",
+      phoneNumber: "7397873082878",
+    },
+    {
+      id: 4,
+      userName: "Uche",
+      phoneNumber: "7397873082878",
+    },
+    {
+      id: 5,
+      userName: "Faith",
+      phoneNumber: "7397873082878",
+    },
+    {
+      id: 6,
+      userName: "Ozumahe",
+      phoneNumber: "7397873082878",
+    },
+  ]);
+
+  //How can you make it more efficient? so it does not calculate duplicates for each time for every user.
+
+  const findDuplicatedUserName = (userName: string) => {
+    let duplicatedUsers = [];
+
+    for (let i = 0; i < users.length; i++) {
+      const element = users[i];
+
+      const existingUser = duplicatedUsers.find(
+        (data) => data.userName === element.userName
+      );
+      const existingUserIndex = duplicatedUsers.findIndex(
+        (data) => data.userName === element.userName
+      );
+
+      if (existingUser) {
+        duplicatedUsers[existingUserIndex].count++;
+      } else {
+        duplicatedUsers.push({ userName: element.userName, count: 1 });
+      }
+    }
+
+    const result = duplicatedUsers.reduce(
+      (acc: string[], value: { count: number; userName: string }) => {
+        if (value.count > 1) {
+          acc.push(value.userName);
+        }
+        return acc;
+      },
+      []
+    );
+
+    if (result.includes(userName)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" style={{}}>
+      {users.map(({ id, userName, phoneNumber }) => (
+        <div key={id}>
+          {findDuplicatedUserName(userName) ? (
+            <div style={{ color: "#F00" }}>X</div>
+          ) : null}
+          <p style={{ color: "" }}>{userName}</p>
+          <p>{phoneNumber}</p>
+        </div>
+      ))}
     </div>
   );
 }
